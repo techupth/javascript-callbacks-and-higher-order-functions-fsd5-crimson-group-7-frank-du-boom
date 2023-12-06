bc7c1c3339c9a16e8b5296d8ef3d448dfc8e1f05
Nitisit Vithunat <nitisitvithunat@MacBook-Pro-khxng-Nitisit.local>
2023-12-06 23:44:39 +0700

    submit callback&hof

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,32 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function atLeastFive(array, operation) {
+  // Start coding here
+  let countScore = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      countScore = countScore + 1;
+    }
+  }
+  return operation(countScore);
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+// Using `atLeastFive` function here
+function amountCheck(amount) {
+  if (amount >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, amountCheck);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, amountCheck);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, amountCheck);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
