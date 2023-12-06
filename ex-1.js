c152e81dfde7eac7bec895f7e1da4cf01126d50f
nattawat <frank.nattawat@hotmail.com>
2023-12-06 19:13:58 +0700

    feat:complete ex callbacks

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,19 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
+  for (let i=0 ; i<employeeSalaries.length ; i++){
+    let result = array[i]+5000;
+    operation(result);
+  }
   // Start coding here
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
+forEach(employeeSalaries, function(newSalaries){
+  newEmployeeSalaries.push(newSalaries);
+})
 // Using `forEach` function here
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,30 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let result = [];
+  for(let i=0 ; i<array.length ; i++){
+    if(operation(array[i])){ 
+      result.push(array[i]);
+    };
+  }
+  return result.length > 4;
+}
+
+function morethan70(score){
+  return score > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+// Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, morethan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, morethan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, morethan70);
+
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
+
